af1a3b377a15502016545e9a4c048bdafa35dd05
Hiras <mhmdhiras@gmail.com>
2018-03-26 15:03:39 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>HackerNews</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background-color: #E64A19;
+}
+
+#app {
+  color: #FAFAFA;
+}
+
+</style>
